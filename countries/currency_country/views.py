ac35ee9c62222d0f8946a5394423ce1470b91f17
Kevin <kevin.peraza@live.com>
2021-04-14 21:48:14 -0600

    Add views

--- countries/currency_country/views.py
+++ countries/currency_country/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Country
+from .serializers import CountrySerializer
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def country(req):
+    if req.method == "GET":
+        return Response(CountrySerializer(Country.objects.all(), many=True).data)
+    elif req.method == "POST":
+        serializer = CountrySerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
